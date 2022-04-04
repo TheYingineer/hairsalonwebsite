34d37534ea764d243855d2076c6b75490a16ee5b
TheYingineer <ying.yan.huang87@gmail.com>
2022-04-03 22:21:44 -0500

    cleaned up menu feature, it works now. :)

---
+++ index.js
@@ -0,0 +1,8 @@
+function OpenMenu() {
+    document.getElementById("menu").style.right = "0%";
+  }
+  
+  function CloseMenu() {
+    document.getElementById("menu").style.right = "-100%";
+  }
+  
